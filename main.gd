2c20e9e76ad5023dada36c384330d0306a17868d
Christopher Pane <chrispanecs@gmail.com>
2025-01-03 21:29:12 -0500

    Add start game button for host

--- main.gd
+++ main.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var host_button = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/HostButton
 @onready var join_button = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/JoinButton
+@onready var start_game_button = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/StartGameButton
+
 @onready var error_label = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/ErrorLabel
 @onready var players_label = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/PlayersLabel
 
@@ -26,15 +28,22 @@ func _ready():
 	# Preconfigure game.
 	Lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
 	
-	host_button.pressed.connect(start_game)
+	host_button.pressed.connect(host_game)
 	join_button.pressed.connect(join_game)
-	Lobby.player_connected.connect(on_player_connected)
+	start_game_button.pressed.connect(start_game)
+	Lobby.player_connected.connect(update_lobby_info)
+	Lobby.player_disconnected.connect(update_lobby_info)
 
-func on_player_connected(new_player_id, new_player_info):
+func update_lobby_info(new_player_id, new_player_info):
 	var res = ""
 	for peer_id in Lobby.players.keys():
-		res += str(peer_id) + "\n"
+		var player_info = Lobby.players[peer_id]
+		res += str(peer_id) + ": " + str(player_info["selected_character_id"]) + "\n"
 	players_label.text = res
+
+func host_game():
+	start_game_button.visible = true
+	Lobby.create_game()
 
 func join_game():
 	if !Lobby.player_info["selected_character_id"]:
